04a8fe9ec7f161666151d915a26b32a898a0ac98
Matt <mknickelbine@gmail.com>
2018-11-13 17:22:12 -0600

    Added touch and mkdir commands

--- terminal-commands.js
+++ terminal-commands.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 
-module.exports.ls = () => {
-  fs.readdir('./', (err, files) => {
-    const filesToString = files.reduce((acc, file) => {
-      return `${acc} ${file} `;
-    }, '');
-    console.log(filesToString);
-  });
-};
-
-module.exports.touch = (fileName) => {
-  fs.writeFile(fileName, '', err => {
-    if (err) throw err;
-    console.log(`New file "${fileName}" created.`);
-  });
-}
-
-module.exports.mkdir = (dirName) => {
-  fs.mkdir(dirName, { recursive: true }, (err) => {
-    if (err) throw err;
-    console.log(`New directory "${dirName}" created.`);
-  });
+module.exports= {
+  ls: () => {
+    fs.readdir('./', (err, files) => {
+      const filesToString = files.reduce((acc, file) => {
+        return `${acc} ${file} `;
+      }, '');
+      console.log(filesToString);
+    });
+  },
+  touch: (fileName) => {
+    fs.writeFile(fileName, '', err => {
+      if (err) throw err;
+      console.log(`New file "${fileName}" created.`);
+    });
+  },
+  mkdir: (dirName) => {
+    fs.mkdir(dirName, { recursive: true }, (err) => {
+      if (err) throw err;
+      console.log(`New directory "${dirName}" created.`);
+    });
+  }
 }
